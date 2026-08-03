b0719ec61260ebf841c6fb76a751028f92ef2d9c
Priyanshu Kumar <143216162+Priyanshav@users.noreply.github.com>
2026-08-03 12:18:31 +0530

    Time: 0 ms (100%), Space: 46.4 MB (36.65%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,27 +10,42 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+        // if(head == null || head.next == null) return head;
+        // List<Integer> arr = new ArrayList<>();
+        // ListNode temp = head;
+        // while(temp != null && temp.next != null){
+        //     arr.add(temp.val);
+        //     temp = temp.next.next;
+        // }
+        // if(temp != null) arr.add(temp.val);
+        // temp = head.next;
+        // while(temp != null && temp.next != null){
+        //     arr.add(temp.val);
+        //     temp = temp.next.next;
+        // }
+        // if(temp != null) arr.add(temp.val);
+        // temp = head;
+        // int num = 0;
+        // while(temp != null){
+        //     temp.val = arr.get(num);
+        //     temp = temp.next;
+        //     num++;
+        // }
+        // return head;
+
         if(head == null || head.next == null) return head;
-        List<Integer> arr = new ArrayList<>();
-        ListNode temp = head;
-        while(temp != null && temp.next != null){
-            arr.add(temp.val);
-            temp = temp.next.next;
+        ListNode odd = head;
+        ListNode evenHead = head.next;
+        ListNode even = evenHead;
+
+        while(even != null && even.next != null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+
+            odd = odd.next;
+            even = even.next;
         }
-        if(temp != null) arr.add(temp.val);
-        temp = head.next;
-        while(temp != null && temp.next != null){
-            arr.add(temp.val);
-            temp = temp.next.next;
-        }
-        if(temp != null) arr.add(temp.val);
-        temp = head;
-        int num = 0;
-        while(temp != null){
-            temp.val = arr.get(num);
-            temp = temp.next;
-            num++;
-        }
+        odd.next = evenHead;
         return head;
     }
 }
