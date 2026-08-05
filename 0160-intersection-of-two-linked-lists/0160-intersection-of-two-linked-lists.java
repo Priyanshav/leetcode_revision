5b7d1f5e9a8787dec4c163900421f9ba23ed7c84
Priyanshu Kumar <143216162+Priyanshav@users.noreply.github.com>
2026-08-05 14:26:06 +0530

    Time: 2 ms (49.4%), Space: 53.2 MB (12.65%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,16 +11,42 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode head1, ListNode head2) {
-        Map<ListNode, Integer> map = new HashMap<>();
-        ListNode temp1 = head1;
+        // Map<ListNode, Integer> map = new HashMap<>();
+        // ListNode temp1 = head1;
 
+        // while(temp1 != null){
+        //     map.put(temp1, 1);
+        //     temp1 = temp1.next;
+        // }
+        // ListNode temp2 = head2;
+        // while(temp2 != null){
+        //     if(map.containsKey(temp2)) return temp2;
+        //     temp2 = temp2.next;
+        // }
+        // return null;
+        int l1 = 0;
+        int l2 = 0;
+        ListNode temp1 = head1;
+        ListNode temp2 = head2;
         while(temp1 != null){
-            map.put(temp1, 1);
+            l1++;
             temp1 = temp1.next;
         }
-        ListNode temp2 = head2;
         while(temp2 != null){
-            if(map.containsKey(temp2)) return temp2;
+            l2++;
+            temp2 = temp2.next;
+        }
+        if(l1 > l2) return moveAhead(head1, head2, l1-l2);
+        else return moveAhead(head2, head1, l2-l1);
+    }
+
+    public ListNode moveAhead(ListNode LL1, ListNode LL2, int N){
+        ListNode temp1 = LL1;
+        ListNode temp2 = LL2;
+        for(int i = 0; i < N; i++) temp1 = temp1.next;
+        while(temp1 != null && temp2 != null){
+            if(temp1 == temp2) return temp1;
+            temp1 = temp1.next;
             temp2 = temp2.next;
         }
         return null;
