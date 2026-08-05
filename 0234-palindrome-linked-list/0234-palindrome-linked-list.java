51c3e550f149e11fad4b3d511b095adf626e8278
Priyanshu Kumar <143216162+Priyanshav@users.noreply.github.com>
2026-08-05 12:32:16 +0530

    Time: 4 ms (66%), Space: 96.6 MB (29.69%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,17 +10,53 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> st = new Stack<>();
-        ListNode temp = head;
-        while(temp != null){
-            st.push(temp.val);
-            temp = temp.next;
+        // Stack<Integer> st = new Stack<>();
+        // ListNode temp = head;
+        // while(temp != null){
+        //     st.push(temp.val);
+        //     temp = temp.next;
+        // }
+        // temp = head;
+        // while(temp != null){
+        //     if(temp.val != st.pop()) return false;
+        //     temp = temp.next;
+        // }
+        // return true;
+
+        if(head == null || head.next == null) return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        temp = head;
-        while(temp != null){
-            if(temp.val != st.pop()) return false;
-            temp = temp.next;
+        ListNode newHead = reverseLL(slow.next);
+        ListNode first = head;
+        ListNode second = newHead;
+
+        while(second != null){
+            if(first.val != second.val){
+                reverseLL(newHead);
+                return false;
+            }
+            first = first.next;
+            second = second.next;
         }
+        reverseLL(newHead);
         return true;
+    }
+    public ListNode reverseLL(ListNode head){
+        ListNode prev = null;
+        ListNode temp = head;
+
+        while(temp != null){
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
     }
 }
