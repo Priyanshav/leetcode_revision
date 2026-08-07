0dd20e1fa7addb095b9c859d28e51d8579899a55
Priyanshu Kumar <143216162+Priyanshav@users.noreply.github.com>
2026-08-07 14:40:10 +0530

    Time: 11 ms (38.43%), Space: 59.4 MB (62.64%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,45 +10,32 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        // List<Integer> arr = new ArrayList<>();
-        // ListNode temp = head;
-        // while(temp != null){
-        //     arr.add(temp.val);
-        //     temp = temp.next;
-        // }
-        // Collections.sort(arr);
-        // temp = head;
-        // for(int i: arr){
-        //     temp.val = i;
-        //     temp = temp.next;
-        // }
-        // return head;
         if(head == null || head.next == null) return head;
         ListNode middle = findMiddle(head);
         ListNode right = middle.next;
         middle.next = null;
         ListNode left = head;
+
         left = sortList(left);
         right = sortList(right);
+
         return merge(left, right);
     }
-
     public ListNode findMiddle(ListNode head){
-        ListNode fast = head.next;
         ListNode slow = head;
+        ListNode fast = head.next;
+
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
         return slow;
     }
-
     public ListNode merge(ListNode l1, ListNode l2){
         ListNode dummy = new ListNode();
         ListNode dHead = dummy;
         ListNode t1 = l1;
         ListNode t2 = l2;
-
         while(t1 != null && t2 != null){
             if(t1.val < t2.val){
                 dHead.next = t1;
