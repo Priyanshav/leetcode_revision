bc8eeea953b595c3ac101d65907a5cb4f48e9b94
Priyanshu Kumar <143216162+Priyanshav@users.noreply.github.com>
2026-08-07 11:51:09 +0530

    Time: 12 ms (31.89%), Space: 59.5 MB (49.13%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,18 +10,66 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        List<Integer> arr = new ArrayList<>();
-        ListNode temp = head;
-        while(temp != null){
-            arr.add(temp.val);
-            temp = temp.next;
+        // List<Integer> arr = new ArrayList<>();
+        // ListNode temp = head;
+        // while(temp != null){
+        //     arr.add(temp.val);
+        //     temp = temp.next;
+        // }
+        // Collections.sort(arr);
+        // temp = head;
+        // for(int i: arr){
+        //     temp.val = i;
+        //     temp = temp.next;
+        // }
+        // return head;
+        if(head == null || head.next == null) return head;
+        ListNode middle = findMiddle(head);
+        ListNode right = middle.next;
+        middle.next = null;
+        ListNode left = head;
+        left = sortList(left);
+        right = sortList(right);
+        return merge(left, right);
+    }
+
+    public ListNode findMiddle(ListNode head){
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        Collections.sort(arr);
-        temp = head;
-        for(int i: arr){
-            temp.val = i;
-            temp = temp.next;
+        return slow;
+    }
+
+    public ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode();
+        ListNode dHead = dummy;
+        ListNode t1 = l1;
+        ListNode t2 = l2;
+
+        while(t1 != null && t2 != null){
+            if(t1.val < t2.val){
+                dHead.next = t1;
+                t1 = t1.next;
+            }
+            else{
+                dHead.next = t2;
+                t2 = t2.next;
+            }
+            dHead = dHead.next;
         }
-        return head;
+        while(t1 != null){
+            dHead.next = t1;
+            t1 = t1.next;
+            dHead = dHead.next;
+        }
+        while(t2 != null){
+            dHead.next = t2;
+            t2 = t2.next;
+            dHead = dHead.next;
+        }
+        return dummy.next;
     }
 }
