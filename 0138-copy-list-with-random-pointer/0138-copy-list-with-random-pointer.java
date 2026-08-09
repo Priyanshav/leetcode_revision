2bce8ed09be8360d7c0f3f6f962f76e64fb98d2c
Priyanshu Kumar <143216162+Priyanshav@users.noreply.github.com>
2026-08-09 21:06:43 +0530

    Time: 0 ms (100%), Space: 46.3 MB (88.94%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,20 +15,44 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Map<Node, Node> map = new HashMap<>();
+        // Map<Node, Node> map = new HashMap<>();
+        // Node temp = head;
+        // while(temp != null){
+        //     Node newNode = new Node(temp.val);
+        //     map.put(temp, newNode);
+        //     temp = temp.next;
+        // }
+        // temp = head;
+        // while(temp != null){
+        //     Node copyNode = map.get(temp);
+        //     copyNode.next = map.get(temp.next);
+        //     copyNode.random = map.get(temp.random);
+        //     temp = temp.next;
+        // }
+        // return map.get(head);
         Node temp = head;
         while(temp != null){
             Node newNode = new Node(temp.val);
-            map.put(temp, newNode);
-            temp = temp.next;
+            newNode.next = temp.next;
+            temp.next = newNode;
+            temp = temp.next.next;
         }
         temp = head;
         while(temp != null){
-            Node copyNode = map.get(temp);
-            copyNode.next = map.get(temp.next);
-            copyNode.random = map.get(temp.random);
-            temp = temp.next;
+            Node copyNode = temp.next;
+            if(temp.random != null) copyNode.random = temp.random.next;
+            else copyNode.random = null;
+            temp = temp.next.next;
         }
-        return map.get(head);
+        temp = head;
+        Node dummy = new Node(-1);
+        Node dHead = dummy;
+        while(temp != null){
+            dHead.next = temp.next;
+            temp.next = temp.next.next;
+            temp = temp.next;
+            dHead = dHead.next;
+        }
+        return dummy.next;
     }
 }
