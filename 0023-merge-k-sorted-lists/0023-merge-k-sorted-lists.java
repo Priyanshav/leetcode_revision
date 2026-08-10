1520e558b8796a33468d9dc21604f1397ef4ee37
Priyanshu Kumar <143216162+Priyanshav@users.noreply.github.com>
2026-08-10 14:49:02 +0530

    Time: 4 ms (80.09%), Space: 47 MB (29%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -13,35 +13,53 @@ class Solution {
         if(lists.length == 0) return null;
         ListNode head = lists[0];
         if(lists.length == 1) return head;
-        for(int i = 1; i < lists.length; i++){
-            head = merge2Lists(head, lists[i]);
+        // for(int i = 1; i < lists.length; i++){
+        //     head = merge2Lists(head, lists[i]);
+        // }
+        // return head;
+        PriorityQueue<ListNode> PQ = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for(ListNode nodes : lists){
+            if(nodes != null) PQ.add(nodes);
         }
-        return head;
-    }
-    public ListNode merge2Lists(ListNode l1, ListNode l2){
+
         ListNode dummy = new ListNode();
-        ListNode dHead = dummy;
-        while(l1 != null && l2 != null){
-            if(l1.val < l2.val){
-                dHead.next = l1;
-                l1 = l1.next;
-            }else{
-                dHead.next = l2;
-                l2 = l2.next;
+        ListNode temp = dummy;
+
+        while(!PQ.isEmpty()){
+            ListNode node = PQ.poll();
+            temp.next = node;
+            temp = temp.next;
+
+            if(node.next != null){
+                PQ.add(node.next);
             }
-            dHead = dHead.next;
-        } 
-        while(l1 != null){
-            dHead.next = l1;
-            l1 = l1.next;
-            dHead = dHead.next;
-        }
-        while(l2 != null){
-            dHead.next = l2;
-            l2 = l2.next;
-            dHead = dHead.next;
         }
         return dummy.next;
     }
-
+    // public ListNode merge2Lists(ListNode l1, ListNode l2){
+    //     ListNode dummy = new ListNode();
+    //     ListNode dHead = dummy;
+    //     while(l1 != null && l2 != null){
+    //         if(l1.val < l2.val){
+    //             dHead.next = l1;
+    //             l1 = l1.next;
+    //         }else{
+    //             dHead.next = l2;
+    //             l2 = l2.next;
+    //         }
+    //         dHead = dHead.next;
+    //     } 
+    //     while(l1 != null){
+    //         dHead.next = l1;
+    //         l1 = l1.next;
+    //         dHead = dHead.next;
+    //     }
+    //     while(l2 != null){
+    //         dHead.next = l2;
+    //         l2 = l2.next;
+    //         dHead = dHead.next;
+    //     }
+    //     return dummy.next;
+    // }
 }
