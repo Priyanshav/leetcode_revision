0e781848ab0656beef461454dd612faa03e7694a
Priyanshu Kumar <143216162+Priyanshav@users.noreply.github.com>
2026-09-09 15:31:38 +0530

    Time: 0 ms (100%), Space: 43.6 MB (69.12%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,7 +13,8 @@
  *     }
  * }
  */
- class Pair{
+/*
+class Pair{
     TreeNode node;
     int level;
 
@@ -22,10 +23,12 @@
         this.level = level;
     }
  }
+ */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         if(root == null) return ans;
+        /*
         Map<Integer, Integer> map = new TreeMap<>();
         Queue<Pair> q = new LinkedList<>();
         q.offer(new Pair(root, 0));
@@ -39,5 +42,14 @@ class Solution {
         }
         for(Integer values : map.values()) ans.add(values);
         return ans;
+        */
+        backtrack(root, 0, ans);
+        return ans;
+    }
+    private void backtrack(TreeNode node, int level, List<Integer> ans){
+        if(node == null) return ;
+        if(level == ans.size()) ans.add(node.val);
+        backtrack(node.right, level+1, ans);
+        backtrack(node.left, level+1, ans);
     }
 }
