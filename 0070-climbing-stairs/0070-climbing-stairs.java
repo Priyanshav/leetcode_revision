93c8f674280a1aa4a1ea5de0615bc6bc73d2af6f
Priyanshu Kumar <143216162+Priyanshav@users.noreply.github.com>
2026-09-20 23:25:21 +0530

    Time: 0 ms (100%), Space: 42 MB (72.95%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,6 +5,7 @@ class Solution {
         Arrays.fill(dp, -1);
         return recursion(n, dp);
         */
+        /*
         dp[0] = 1;
         dp[1] = 1;
 
@@ -12,6 +13,16 @@ class Solution {
             dp[i] = dp[i-1] + dp[i-2];
         }
         return dp[n];
+        */
+        int prev1 = 1;
+        int prev2 = 1;
+
+        for(int i = 2; i <= n; i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
     /*
     private int recursion(int n, int[] dp){
