e7e3a48c00ebbd3e12b7b337e3564d1ddbfa96bc
Priyanshu Kumar <143216162+Priyanshav@users.noreply.github.com>
2026-09-24 23:05:15 +0530

    Time: 0 ms (100%), Space: 43.1 MB (8.5%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,17 +1,33 @@
 class Solution {
+    private int func(int[] arr, int start, int end){
+        int prev2 = arr[0];
+        int prev1 = 0;
+        for(int i = start; i < end; i++){
+            int pick = arr[i];
+            if(i > 1) pick += prev2;
+            int notPick = prev1;
+            int curr = Math.max(pick, notPick);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
     public int rob(int[] nums) {
         if(nums.length == 0) return 0;
         if(nums.length == 1) return nums[0];
         int n = nums.length-1;
+        int ans1 = func(nums, 0, n);
+        int ans2 = func(nums, 1, n+1);
+        return Math.max(ans1, ans2);
+        /*
         int[] temp1 = new int[n];
         int[] temp2 = new int[n];
-
-        
         for(int i = 0; i <=  n; i++){
             if(i != 0) temp1[i-1] = nums[i];
             if(i != n) temp2[i] = nums[i];
         }
         return Math.max(func(temp1), func(temp2));
+        */
         /*
         int[] dp1 = new int[n+1];
         int[] dp2 = new int[n+1];
@@ -23,6 +39,8 @@ class Solution {
         return Math.max(call1, call2);
         */
     }
+    /*
+    Tabulation
     private int func(int[] arr){
         int n = arr.length - 1;
         int[] dp = new int[n+1];
@@ -35,6 +53,7 @@ class Solution {
         }
         return dp[n];
     }
+    */
     /*
     Memoization
     private int func(int n, int[] arr, int[] dp){
