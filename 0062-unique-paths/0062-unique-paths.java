cb1056eac105c99fbf598d8c3f0c1fabef74f5d0
Priyanshu Kumar <143216162+Priyanshav@users.noreply.github.com>
2026-09-25 23:53:53 +0530

    Time: 0 ms (100%), Space: 41.6 MB (97.49%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -2,11 +2,27 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                    int up = 0;
+                    int left = 0;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+        /*
+        for(int i = 0; i < m; i++){
             Arrays.fill(dp[i], -1);
         }
         return func(m-1, n-1, dp);
+        */
     }
-
+    /*
+    Memoization
     private int func(int i, int j, int[][] dp){
         if(i == 0 && j == 0) return 1;
         if(i < 0 || j < 0) return 0;
@@ -15,4 +31,5 @@ class Solution {
         int up = func(i-1, j, dp);
         return dp[i][j] = left + up;
     }
+    */
 }
